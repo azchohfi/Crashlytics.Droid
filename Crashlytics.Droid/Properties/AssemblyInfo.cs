144f5666029f5d2fb5e9de270639d90c07c161c8
Alexandre Chohfi <azchohfi@hotmail.com>
2017-12-28 13:41:27 -0800

    Updated to crashlytics 2.8.0.

--- Crashlytics.Droid/Properties/AssemblyInfo.cs
+++ Crashlytics.Droid/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Crashlytics.Droid")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
